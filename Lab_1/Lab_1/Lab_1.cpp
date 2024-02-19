c14ff813f31e5508305a7b60788aea027ebfd72c
DerhachovaLM <159348213+DerhachovaLM@users.noreply.github.com>
2024-02-19 23:57:33 +0200

    Add files via upload

---
+++ Lab_1/Lab_1/Lab_1.cpp
@@ -0,0 +1,42 @@
+﻿
+#include <iostream>
+#include <Windows.h>
+int main()
+
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    std::cout << "Hello World!!!\n";
+
+    std::cout << "\n\n\n";
+
+    std::cout << "* * * * * *\n";
+    std::cout << "*         *\n";
+    std::cout << "*         *\n";
+    std::cout << "*         *\n";
+    std::cout << "*         *\n";
+    std::cout << "* * * * * *\n";
+
+    std::cout << "\n\n\n";
+
+    std::cout << "     /\\\n";
+    std::cout << "    /  \\\n";
+    std::cout << "   /    \\\n";
+    std::cout << "  /      \\\n";
+    std::cout << " /        \\\n";
+    std::cout << " ..........\n";
+
+    std::cout << "\n\n\n";
+
+    std::cout << "       Прізвище : Derhachova\n";
+    std::cout << "           Ім'я : Lidia\n";
+    std::cout << "   По батьковій : Mikhailivna\n";
+    std::cout << "          Стать : Жіноча\n";
+    std::cout << "Дата народження : 20-04-2006\n";
+    std::cout << "         Адреса : *******\n";
+    std::cout << " Номер телефону : *********\n";
+    std::cout << " Місце навчення : ММФ, ДНУ ім. Олеся Гончара\n";
+    std::cout << "          Група : МС-23-1\n";
+    std::cout << "           Хоббі: Заварювати чай\n";
+}
+
